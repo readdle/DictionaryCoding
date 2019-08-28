5b46729e187c7af4dd6315201d5c9f3e5f1221e1
Viktor Gedzenko <fox@readdle.com>
2019-08-28 16:27:27 +0300

    Fix package

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "DictionaryCoding",
-            targets: ["DictionaryCoding"]),
+            name: "RDDictionaryCoding",
+            targets: ["RDDictionaryCoding"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "DictionaryCoding",
-            dependencies: []),
+            name: "RDDictionaryCoding",
+            dependencies: [],
+			path: "Sources/DictionaryCoding"),
         .testTarget(
-            name: "DictionaryCodingTests",
-            dependencies: ["DictionaryCoding"]),
+            name: "RDDictionaryCodingTests",
+            dependencies: ["RDDictionaryCoding"],
+			path: "Tests/DictionaryCodingTests"),
     ]
 )
